6483fd44d1cd2b91f1e024e47e601d00014eca2a
João Victor Nogueira Doratioto <jvdoratioto@gmail.com>
2023-07-05 20:21:16 -0300

    html projeto

---
+++ dsnv-web-1/projeto_01/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <meta name="description" content="Descrição do Website">
+    <title>Projeto 01</title>
+    <link rel="stylesheet" href="estilo/style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;0,800;1,300;1,700;1,800&display=swap');
+    </style>
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="center">
+            <div class="logo">Logomarca</div> <!-- LOGO -->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="Mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div> <!--  center -->
+    </header>
+
+    <section class="banner-principal">
+
+        <div class="center">
+        <form action="" method="post">
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" id="iemail" required>
+            <input type="submit" value="Cadastrar" name="acao">
+        </form>
+        </div> <!-- center -->
+
+    </section> <!-- BANNER PRINCIPAL -->
+
+    <section class="descricao-autor">
+
+        <div class="center">
+        <div class="w50">
+            <h2>João victor N. D.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, dignissimos dolor! Dignissimos iusto vel eius ad dolorum dolor tempora dolorem. Asperiores sit omnis quidem quasi sequi obcaecati blanditiis inventore modi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut id, dolore deleniti ab aliquam nostrum laborum vero officiis cupiditate minus, doloribus alias tenetur. Odio fugiat quidem sint architecto eaque aspernatur!</p>
+            
+        </div> <!-- W50 -->  
+        <div class="w50">
+            <!-- PEGAR IMG DPS -->
+            <img src="imagens/foto1.jpeg" alt="imagem">
+        </div> <!-- W50 -->
+        </div> <!-- center -->
+
+    </section><!-- DESCRIÇÃO-AUTOR -->
+
+    <section class="especialidades">
+        
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box-especialidades">
+                <h3><i class="fa-brands fa-css3-alt"></i></h3>
+                <h3>CSS3</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem assumenda id delectus sunt commodi rerum harum aliquam libero cum magni in, omnis sequi ipsa at, blanditiis, ipsum porro alias architecto?</p>
+            </div><!-- box-especialidades -->
+            <div class="box-especialidades">
+                <h3><i class="fa-brands fa-html5"></i></h3>
+                <h3>HTML5</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptates, minima laudantium beatae unde dolorum. In hic explicabo harum illo non tempora ducimus eum quibusdam fugit, rem facilis dolore veritatis!</p>
+            </div><!-- box-especialidades -->
+            <div class="box-especialidades">
+                <h3><i class="fa-brands fa-js"></i></h3>
+                <h3>Javascript</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti consequuntur atque, hic quod earum minima eius laudantium ab quo culpa inventore fugit quibusdam. Sequi a reiciendis itaque ullam eaque nulla!</p>
+            </div><!-- box-especialidades -->
+        </div> <!-- center -->
+
+    </section> <!-- ESPECIALIDADES -->
+
+    <section class="extras">
+
+        <div class="center">
+            <div class="w50">
+                <h2 class="tittle">Depoimentos</h2>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descrição">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!-- depoimento-single -->
+                <div class="depoimentos-single">
+                    <p class="depoimento-descrição">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!-- depoimento-single -->
+                <div class="depoimentos-single">
+                    <p class="depoimento-descrição">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!-- depoimento-single -->
+            </div><!-- w50 -->
+            <div class="w50">
+                <h2 class="tittle">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum obcaecati tempora ut quas! Omnis, earum. Ex aliquam id, magni facilis fuga doloribus accusantium reiciendis adipisci debitis, ratione molestias minima molestiae.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minima similique, expedita ducimus aliquam sequi asperiores odio quod dolorum mollitia sint facilis magnam cupiditate nisi, numquam eveniet nulla quaerat vero.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum explicabo saepe, eligendi eius necessitatibus aut tempore laboriosam voluptatem quos odio nisi quaerat aliquid quisquam vitae error quasi nobis? Possimus, quia?</li>
+                    </ul>
+                </div><!-- Servicos -->
+            </div><!-- w50 -->
+        </div><!-- center -->
+
+    </section>
+
+    <footer>
+        <div class="center">
+        <p>Todos os direitos reservados </p>
+        </div><!-- center -->
+    </footer>
+
+
+<script src="https://kit.fontawesome.com/d709ea726d.js" crossorigin="anonymous"></script>
+</body>
+</html>
