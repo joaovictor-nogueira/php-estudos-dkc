65f1ffbd1e2cfac2841c45751062bab9272f2b79
João Victor Nogueira Doratioto <jvdoratioto@gmail.com>
2023-07-05 23:05:30 -0300

    css do site

--- dsnv-web-1/projeto_01/index.php
+++ dsnv-web-1/projeto_01/index.php
@@ -15,8 +15,8 @@
 <body>
     <header>
         <div class="center">
-            <div class="logo">Logomarca</div> <!-- LOGO -->
-            <nav class="desktop">
+            <div class="logo left">Logomarca</div> <!-- LOGO -->
+            <nav class="desktop right">
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
@@ -24,7 +24,8 @@
                     <li><a href="">Contato</a></li>
                 </ul>
             </nav>
-            <nav class="Mobile">
+            <nav class="mobile right">
+                <div class="botao-menu-mobile"><i class="fa-solid fa-bars"></i></div>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
@@ -32,16 +33,17 @@
                     <li><a href="">Contato</a></li>
                 </ul>
             </nav>
+        <div class="clear"></div><!-- clear -->
         </div> <!--  center -->
     </header>
 
     <section class="banner-principal">
-
+    <div class="overlay"></div><!-- overlay -->
         <div class="center">
         <form action="" method="post">
             <h2>Qual o seu melhor e-mail?</h2>
             <input type="email" name="email" id="iemail" required>
-            <input type="submit" value="Cadastrar" name="acao">
+            <input type="submit" value="Cadastrar!" name="acao">
         </form>
         </div> <!-- center -->
 
@@ -50,16 +52,20 @@
     <section class="descricao-autor">
 
         <div class="center">
-        <div class="w50">
+        <div class="w50 left">
             <h2>João victor N. D.</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, dignissimos dolor! Dignissimos iusto vel eius ad dolorum dolor tempora dolorem. Asperiores sit omnis quidem quasi sequi obcaecati blanditiis inventore modi.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut id, dolore deleniti ab aliquam nostrum laborum vero officiis cupiditate minus, doloribus alias tenetur. Odio fugiat quidem sint architecto eaque aspernatur!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, saepe. At recusandae unde sunt odio molestiae odit? Nam dolor dignissimos neque, reprehenderit laboriosam
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis architecto aperiam magnam ducimus repudiandae aspernatur saepe fugiat corrupti beatae sapiente doloribus labore, vero, unde quas ullam dicta consectetur, voluptatibus libero.
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nostrum delectus dolorem accusamus architecto, doloremque consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illo ducimus enim, minima ab non in laboriosam at dolorem ut optio, provident voluptatibus totam rerum nobis. Iure, maiores laborum. Reprehenderit.</p>
+            
             
         </div> <!-- W50 -->  
-        <div class="w50">
+        <div class="w50 left">
             <!-- PEGAR IMG DPS -->
-            <img src="imagens/foto1.jpeg" alt="imagem">
+            <img class="right" src="imagens/foto1.jpeg" alt="imagem">
         </div> <!-- W50 -->
+        <div class="clear"></div>
         </div> <!-- center -->
 
     </section><!-- DESCRIÇÃO-AUTOR -->
@@ -67,22 +73,23 @@
     <section class="especialidades">
         
         <div class="center">
-            <h2 class="title">Especialidades</h2>
-            <div class="box-especialidades">
+            <h2 class="title">Minhas Especialidades</h2>
+            <div class="w33 left box-especialidade">
                 <h3><i class="fa-brands fa-css3-alt"></i></h3>
-                <h3>CSS3</h3>
+                <h4>CSS3</h4>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem assumenda id delectus sunt commodi rerum harum aliquam libero cum magni in, omnis sequi ipsa at, blanditiis, ipsum porro alias architecto?</p>
             </div><!-- box-especialidades -->
-            <div class="box-especialidades">
+            <div class="w33 left box-especialidade">
                 <h3><i class="fa-brands fa-html5"></i></h3>
-                <h3>HTML5</h3>
+                <h4>HTML5</h4>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptates, minima laudantium beatae unde dolorum. In hic explicabo harum illo non tempora ducimus eum quibusdam fugit, rem facilis dolore veritatis!</p>
             </div><!-- box-especialidades -->
-            <div class="box-especialidades">
+            <div class="w33 left box-especialidade">
                 <h3><i class="fa-brands fa-js"></i></h3>
-                <h3>Javascript</h3>
+                <h4>Javascript</h4>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti consequuntur atque, hic quod earum minima eius laudantium ab quo culpa inventore fugit quibusdam. Sequi a reiciendis itaque ullam eaque nulla!</p>
             </div><!-- box-especialidades -->
+            <div class="clear"></div>
         </div> <!-- center -->
 
     </section> <!-- ESPECIALIDADES -->
@@ -90,22 +97,22 @@
     <section class="extras">
 
         <div class="center">
-            <div class="w50">
-                <h2 class="tittle">Depoimentos</h2>
+            <div class="w50 left depoimentos-container">
+                <h2 class="tittle">Depoimentos dos nossos clientes</h2>
                 <div class="depoimentos-single">
-                    <p class="depoimento-descrição">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel.</p>
+                    <p class="depoimento-descrição">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel."</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!-- depoimento-single -->
                 <div class="depoimentos-single">
-                    <p class="depoimento-descrição">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel.</p>
+                    <p class="depoimento-descrição">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel."</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!-- depoimento-single -->
                 <div class="depoimentos-single">
-                    <p class="depoimento-descrição">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel.</p>
+                    <p class="depoimento-descrição">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic fugiat ratione beatae? Non aliquid ut dolorem eligendi voluptatum pariatur, tenetur placeat nisi fugit nostrum explicabo optio quos! Ipsam, vel."</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!-- depoimento-single -->
             </div><!-- w50 -->
-            <div class="w50">
+            <div class="w50 left servicos-container">
                 <h2 class="tittle">Serviços</h2>
                 <div class="servicos">
                     <ul>
@@ -115,6 +122,7 @@
                     </ul>
                 </div><!-- Servicos -->
             </div><!-- w50 -->
+            <div class="clear"></div>
         </div><!-- center -->
 
     </section>
